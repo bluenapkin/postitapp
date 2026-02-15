fd099d99377d1b269aa8ff974234b09aba205cc6
Nawaf <nawaf@example.com>
2026-02-15 21:59:51 +0400

    Initial full project (client + server)

---
+++ client/src/Components/Home.js
@@ -0,0 +1,15 @@
+import logo from "../Images/logo-t.png";
+import Posts from "./Posts";
+import SharePosts from "./SharePost";
+import User from "./User";
+import { Container, Row, Col } from "reactstrap"; //import the Reactstrap Components
+
+const Home = () => {
+  return (
+    <div>
+      <h1> home page</h1>
+    </div>
+  );
+};
+
+export default Home;
