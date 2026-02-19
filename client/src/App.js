b6489553ed1d9b2122a8c87c0b806cc03a5ae585
Nawaf <nawaf@example.com>
2026-02-19 14:27:30 +0400

    Update App and dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
+
 import "./App.css";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
+/*import { BrowserRouter as Router, Routes, Route } from "react-router-dom";*/
+import { Container, Row, Col } from "reactstrap";
+import Footer from "./Components/Home";
 
 const App = () => {
   return (
     <>
-      <Home />
+      <Container>
+        <Row>
+          <Header></Header>
+
+        </Row>
+        <Row className="main">
+          <Home></Home>
+
+        </Row>
+        <Row>
+          <Footer></Footer>
+
+        </Row>
+      </Container>
     </>
   );
 };
